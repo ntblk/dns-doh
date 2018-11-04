ba54a9fcfa90505c7cf419f54a4cae37cb69f809
gurshabad <shabadgrover@gmail.com>
2018-11-04 10:32:01 +0530

    update web tests

---
+++ web/js/servers.js
@@ -0,0 +1,11 @@
+var serverList = [
+	{"dname": "Cloudflare", "url": "https://cloudflare-dns.com/dns-query"},
+	{"dname": "Google", "url": "https://dns.google.com/resolve"},
+	{"dname": "Quad9", "url": "https://dns9.quad9.net/dns-query"},
+	{"dname": "PowerDNS", "url": "https://doh.powerdns.org"},
+	{"dname": "BlahDNS Japan",	"url": "https://doh.blahdns.com/dns-query"},
+	{"dname": "NekomimiRouter.com", "url": "https://dns.dns-over-https.com/dns-query"},
+	{"dname": "SecureDNS",	"url": "https://doh.securedns.eu/dns-query"},
+	{"dname": "Rubyfish.cn", "url": "https://doh.securedns.eu/dns-query"},
+	{"dname": "Commons Host", "url": "https://commons.host"},
+	{"dname": "CleanBrowsing", "url": "https://doh.cleanbrowsing.org/doh/family-filter/"}]
